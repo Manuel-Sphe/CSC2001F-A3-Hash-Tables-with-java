9d09c8fec9919c1fb0a20a82451f5fd995a2e5f0
Sphesihle Madonsela <49991613+Linusihle@users.noreply.github.com>
2022-04-11 20:53:04 +0200

    Using nested loop to generate the combinatios

--- data.py
+++ data.py
@@ -1,21 +1,33 @@
 # Sphesihle Madonsela
 # This create a file with rando 5^9 lines
-
-import random
-def Solution():
-    inner = list()
-    file = open("Data2.txt","w")
-
+        
+def main():
+    
+    data = [0,1,2,3,4]
+    
+    file = open("InputFile.txt",'w')
+    
     string = ''
-    for i in range(0, 5**9):
-        for j in range(0,9):
-            inner.append(str(random.randint(0,4)))
-        file.write(""+"".join(inner)+"\n")
-
-        inner = list()
+    for n1 in data:
+        for n2 in data:
+            for n3 in data:
+                for n4 in data:
+                    for n5 in data:
+                        for n6 in data:
+                            for n7 in data:
+                                for n8 in data:
+                                    for n9 in data:
+                                        string += str(n1) +str(n2) + str(n3) + str(n4)+ str(n5) + str(n6)+str(n7) + str(n8) + str(n9) 
+                                        file.write(string+'\n') # write doesn't have a newline char
+                                        string =''
+                                        
+    print("All Possible combinations are in the InputFile.txt")
+ 
+                                        
 if __name__ == '__main__':
     
-    Solution()
+    # stores the data in a text file 
+    main()    
     
 
 
